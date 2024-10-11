e27f1b2de9b02ee2298986a96d41fee671d2bd3d
fenroe <scrambles9494@gmail.com>
2024-10-11 21:30:14 +0300

    Defined stylesheet for homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#25292e"
+    },
+    text: {
+      color: "#fff"
+    }
+  })
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
     </View>
   );
 }
